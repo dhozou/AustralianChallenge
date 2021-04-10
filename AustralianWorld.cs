3065e4f96311d600414551b85d330c1c520105a3
Dhozou <81537827+dhozou@users.noreply.github.com>
2021-04-09 23:13:01 -0400

    generate platform under spawn point

---
+++ AustralianWorld.cs
@@ -0,0 +1,17 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AustralianChallenge
+{
+	public class AustralianWorld : ModWorld
+	{
+		public override void PostWorldGen()
+		{
+			for (int x = Main.spawnTileX - 2; x < Main.spawnTileX + 2; x++)
+			{
+				WorldGen.PlaceTile(x, Main.spawnTileY - 5, TileID.Cloud, forced: true);
+			}
+		}
+	}
+}
