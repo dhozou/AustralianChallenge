afa8cf45e67832653b3d1a409c4ced052943553f
Dhozou <81537827+dhozou@users.noreply.github.com>
2021-04-18 00:41:43 -0400

    make spawn platform five wide, as intended

--- AustralianWorld.cs
+++ AustralianWorld.cs
@@ -7,7 +7,7 @@ namespace AustralianChallenge
 	public class AustralianWorld : ModWorld
 	{
 		public override void PostWorldGen() {
-			for (int x = Main.spawnTileX - 2; x < Main.spawnTileX + 2; x++) {
+			for (int x = Main.spawnTileX - 2; x <= Main.spawnTileX + 2; x++) {
 				WorldGen.PlaceTile(x, Main.spawnTileY - 5, TileID.Cloud, forced: true);
 			}
 		}
