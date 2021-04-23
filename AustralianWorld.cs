e771c83d114926c6a4b7a837336a38857d9884bf
Dhozou <81537827+dhozou@users.noreply.github.com>
2021-04-22 22:43:11 -0400

    generate spawn platform before trees

--- AustralianWorld.cs
+++ AustralianWorld.cs
@@ -1,15 +1,25 @@
+using System;
+using System.Collections.Generic;
 using Terraria;
+using Terraria.GameContent.Generation;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.World.Generation;
 
 namespace AustralianChallenge
 {
 	public class AustralianWorld : ModWorld
 	{
-		public override void PostWorldGen() {
-			for (int x = Main.spawnTileX - 2; x <= Main.spawnTileX + 2; x++) {
-				WorldGen.PlaceTile(x, Main.spawnTileY - 5, TileID.Cloud, forced: true);
-			}
+		public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight) {
+			var index = tasks.FindIndex(pass => pass.Name == "Spawn Point");
+			if (index == -1)
+				throw new Exception("Failed to insert \"Spawn Platform\" generation pass.");
+
+			tasks.Insert(index + 1, new PassLegacy("Spawn Platform", _ => {
+				for (int x = Main.spawnTileX - 2; x <= Main.spawnTileX + 2; x++) {
+					WorldGen.PlaceTile(x, Main.spawnTileY - 5, TileID.Cloud, forced: true);
+				}
+			}));
 		}
 	}
 }
