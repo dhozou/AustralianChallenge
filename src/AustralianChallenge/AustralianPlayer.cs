6667b9a934efbe14c48c0ff9fadbacce2fb106f0
Dhozou <81537827+dhozou@users.noreply.github.com>
2022-07-23 17:34:24 -0400

    Revert "place platforms upside down to match player orientation"
    This reverts commit b1af19e21c5362edcd0cb2397c2b100e4bc86819.
    
    Fixes #9.

--- src/AustralianChallenge/AustralianPlayer.cs
+++ src/AustralianChallenge/AustralianPlayer.cs
@@ -3,7 +3,6 @@ using MonoMod.Cil;
 using System;
 using Terraria;
 using Terraria.DataStructures;
-using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace AustralianChallenge
@@ -31,23 +30,6 @@ namespace AustralianChallenge
 				c.EmitDelegate<Action<Player>>(player => {
 					if (player.gravDir == -1f)
 						player.KillMe(PlayerDeathReason.ByCustomReason(player.name + " was swallowed by the sky."), 10.0, 0);
-				});
-			};
-
-			// WORKAROUND tModLoader/tModLoader#2724
-			// can't directly hook Player.PlaceThing_Tiles_PlaceIt so add
-			// this logic to the start of an existing nested method instead
-			IL.Terraria.Player.PlaceThing_Tiles_PlaceIt_SpinSmartPlatform += il => {
-				// place platforms upside down
-				var c = new ILCursor(il);
-				c.Emit(OpCodes.Ldarg_0);
-				c.EmitDelegate<Action<Player>>(player => {
-					var tileID = player.inventory[player.selectedItem].createTile;
-					if (player.gravDir == 1f || tileID < 0 || !TileID.Sets.Platforms[tileID])
-						return;
-					WorldGen.PoundTile(Player.tileTargetX, Player.tileTargetY);
-					if (Main.netMode == 1)
-						NetMessage.SendData(msgType: MessageID.TileManipulation, number: 7, number2: Player.tileTargetX, number3: Player.tileTargetY, number4: 1f);
 				});
 			};
 
@@ -110,23 +92,6 @@ namespace AustralianChallenge
 					i => i.MatchLdcR4(1f),
 					i => i.MatchStfld<Player>(nameof(Player.gravDir)));
 				c.RemoveRange(3);
-			};
-
-			IL.Terraria.WorldGen.ReplaceTile_DoActualReplacement_Single += il => {
-				// place platforms upside down when replacing
-				var c = new ILCursor(il);
-				var label = default(ILLabel);
-				c.GotoNext(
-					i => i.MatchLdloc(1),
-					i => i.MatchBrfalse(out label));
-				c.GotoLabel(label);
-				c.Emit(OpCodes.Ldloc_0);
-				c.Emit(OpCodes.Ldarg_0);
-				c.Emit(OpCodes.Ldarg_S, (byte)4);
-				c.EmitDelegate<Action<int, int, Tile>>((oldTileID, newTileID, tile) => {
-					if (Main.LocalPlayer.gravDir == -1f && !TileID.Sets.Platforms[oldTileID] && TileID.Sets.Platforms[newTileID] && tile.Slope == SlopeType.Solid)
-						tile.IsHalfBlock = true;
-				});
 			};
 		}
 	}
